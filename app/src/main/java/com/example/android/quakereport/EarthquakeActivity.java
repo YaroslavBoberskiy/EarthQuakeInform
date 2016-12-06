1849d299c727a14e1b0b2f51ab39466da9c2c87f
Yaroslav <yaroslav.boberskiy@gmail.com>
2016-12-06 17:11:29 +0200

    1. Modified QueryUtils Class. Wiped out placeholder test Json string and added private static methods to fetch Json data from the web. 2. Created AsyncTask class in EarthquakeActivity class to perform Network thread in background.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,31 +17,45 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=2";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Get the list of earthquake locations.
-        final ArrayList<ForecastContent> earthquakes = QueryUtils.extractEarthquakes();
+        QuakeAsyncTask quakeAsyncTask = new QuakeAsyncTask();
+        try {
+            quakeAsyncTask.execute(formURL(USGS_REQUEST_URL));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    private void updateUi(final ArrayList<ForecastContent> eatherquakes) {
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a custom {@link ForecastListAdapter} of earthquakes
-        ForecastListAdapter forecastAdapter = new ForecastListAdapter(this, earthquakes);
+        ForecastListAdapter forecastAdapter = new ForecastListAdapter(this, eatherquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -49,7 +63,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ForecastContent forecastContent = earthquakes.get(position);
+                ForecastContent forecastContent = eatherquakes.get(position);
                 String url = forecastContent.getQuakeInfoUrl();
                 Intent browser = new Intent(Intent.ACTION_VIEW);
                 browser.setData(Uri.parse(url));
@@ -58,6 +72,28 @@ public class EarthquakeActivity extends AppCompatActivity {
         });
 
         earthquakeListView.setAdapter(forecastAdapter);
-
     }
+
+    private static URL formURL (String urlAddress) throws MalformedURLException {
+        URL url = null;
+        if (TextUtils.isEmpty(urlAddress)) {
+            return null;
+        }
+        url = new URL(urlAddress);
+        return url;
+    }
+
+    private class QuakeAsyncTask extends AsyncTask<URL,Void,ArrayList<ForecastContent>> {
+
+        @Override
+        protected ArrayList<ForecastContent> doInBackground(URL... params) {
+            return QueryUtils.extractEarthquakes(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<ForecastContent> forecastContents) {
+            updateUi(forecastContents);
+        }
+    }
+
 }
